5a9ea1fc87081ef9b4879ed82233d3fbf7fa55c3
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2024-12-28 10:30:00 +0530

    adding rotate an array

---
+++ dec-28/rotateArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void rotateArray(int arr[], int n, int k) {
+    k = k % n;
+
+    reverse(arr, arr + n);
+    reverse(arr, arr + k);
+    reverse(arr + k, arr + n);
+}
+
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 2;
+
+    rotateArray(arr, n, k);
+
+    cout << "Rotated Array: ";
+    display(arr, n);
+
+    return 0;
+}
